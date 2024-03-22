1730d26cc9c6aed0a664c781b24afe48d4d52288
Navaneeth1797 <rnavaneethk1797@gmail.com>
2024-03-22 15:16:22 +0530

    changes made again

--- backend/handle/handle.js
+++ backend/handle/handle.js
@@ -1,22 +1,49 @@
+// import mongoose from "mongoose";
+// import dbProducts from "./data.js";
+// import Products from "../models/products.js";
+
+// let handleProducts = async () => {
+//     try {
+//         await mongoose.connect(
+//               "mongodb://rnavaneethk1797:navaneeth@ac-o42eblf-shard-00-00.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-01.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-02.dnthuz8.mongodb.net:27017/?ssl=true&replicaSet=atlas-c7wkf5-shard-0&authSource=admin&retryWrites=true&w=majority&appName=oasis"
+//             // "mongodb://localhost:27017/kitchen-oasis"
+//         );
+//         await Products.deleteMany()
+//         console.log("products are deleted");
+//         await Products.insertMany(dbProducts)
+//         console.log("products are inserted")
+//         process.exit();
+//     } catch (error) {
+//         console.log(error.message);
+//         process.exit();
+//     }
+// }
+
+// handleProducts()
+
 import mongoose from "mongoose";
 import dbProducts from "./data.js";
 import Products from "../models/products.js";
 
-let handleProducts = async () => {
-    try {
-        await mongoose.connect(
-              "mongodb://rnavaneethk1797:navaneeth@ac-o42eblf-shard-00-00.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-01.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-02.dnthuz8.mongodb.net:27017/?ssl=true&replicaSet=atlas-c7wkf5-shard-0&authSource=admin&retryWrites=true&w=majority&appName=oasis"
-            // "mongodb://localhost:27017/kitchen-oasis"
-        );
-        await Products.deleteMany()
-        console.log("products are deleted");
-        await Products.insertMany(dbProducts)
-        console.log("products are inserted")
-        process.exit();
-    } catch (error) {
-        console.log(error.message);
-        process.exit();
-    }
-}
+const handleProducts = async () => {
+  try {
+    await mongoose.connect(
+      process.env.DB_URI ||
+        "mongodb://rnavaneethk1797:navaneeth@ac-o42eblf-shard-00-00.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-01.dnthuz8.mongodb.net:27017,ac-o42eblf-shard-00-02.dnthuz8.mongodb.net:27017/?ssl=true&replicaSet=atlas-c7wkf5-shard-0&authSource=admin&retryWrites=true&w=majority&appName=oasis",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    await Products.deleteMany();
+    console.log("Products are deleted");
+    await Products.insertMany(dbProducts);
+    console.log("Products are inserted");
+    process.exit();
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
-handleProducts()
+handleProducts();
